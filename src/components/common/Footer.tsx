ce491a34b76002f68c70d8ffc09b6f8592c4f183
YhonJ8a <105899541+YhonJ8a@users.noreply.github.com>
2024-09-03 18:34:48 -0500

    creacion y estilado de Navbar y footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+// src/components/common/Footer.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
-export default function Footer() {
-  return (
-    <h1>Footer</h1>
-  )
-}
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__content">
+                <div className="footer__links">
+                    <Link to="/" className="footer__link">Home</Link>
+                    <Link to="/about" className="footer__link">About</Link>
+                    <Link to="/dashboard" className="footer__link">Dashboard</Link>
+                </div>
+                <div className="footer__social">
+                    <a href="https://facebook.com" className="footer__social-link" target="_blank" rel="noopener noreferrer">
+                        Facebook
+                    </a>
+                    <a href="https://twitter.com" className="footer__social-link" target="_blank" rel="noopener noreferrer">
+                        Twitter
+                    </a>
+                    <a href="https://instagram.com" className="footer__social-link" target="_blank" rel="noopener noreferrer">
+                        Instagram
+                    </a>
+                </div>
+            </div>
+            <div className="footer__copyright">
+                &copy; {new Date().getFullYear()} MyApp. All rights reserved.
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,33 +5,32 @@ import './Navbar.css';
 const Navbar: React.FC = () => {
     return (
         <>
-        <header className='navbar'>
-            buenas
-        </header>
-        <nav className="navbar">
-            <div className="navbar__brand">
-                <Link to="/" className="navbar__brand-link">
-                    MyApp
-                </Link>
-            </div>
-            <ul className="navbar__links">
-                <li className="navbar__item">
-                    <NavLink to="/" className="navbar__link" activeClassName="active">
-                        Home
-                    </NavLink>
-                </li>
-                <li className="navbar__item">
-                    <NavLink to="/about" className="navbar__link" activeClassName="active">
-                        About
-                    </NavLink>
-                </li>
-                <li className="navbar__item">
-                    <NavLink to="/dashboard" className="navbar__link" activeClassName="active">
-                        Dashboard
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
+            <header className='navbar'>
+                <nav className="navbar">
+                    <div className="navbar__brand">
+                        <Link to="/" className="navbar__brand-link">
+                            MyApp
+                        </Link>
+                    </div>
+                    <ul className="navbar__links">
+                        <li className="navbar__item">
+                            <NavLink to="/" className="navbar__link" activeClassName="active">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="navbar__item">
+                            <NavLink to="/about" className="navbar__link" activeClassName="active">
+                                About
+                            </NavLink>
+                        </li>
+                        <li className="navbar__item">
+                            <NavLink to="/dashboard" className="navbar__link" activeClassName="active">
+                                Dashboard
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         </>
     );
 };
